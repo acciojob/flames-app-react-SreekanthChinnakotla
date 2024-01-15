a84eeb2c176a65e1b3d976e643b8e9b34fc7d59c
SreekanthChinnakotla <134213780+SreekanthChinnakotla@users.noreply.github.com>
2024-01-15 09:29:30 +0000

    6

--- src/components/flames.js
+++ src/components/flames.js
@@ -1,39 +1,67 @@
 import React, { useState } from 'react';
 
 const FlamesGame = () => {
-  const [name1, setName1] = useState('');
-  const [name2, setName2] = useState('');
-  const [result, setResult] = useState('');
-
-  const calculateFlames = () => {
-    const commonLetters = getCommonLetters(name1, name2);
-    const remainingLetters = name1.length + name2.length - 2 * commonLetters.length;
-    setResult(getFlamesResult(remainingLetters));
-  };
-
-  const getCommonLetters = (str1, str2) => {
-    const arr1 = str1.toLowerCase().replace(/\s/g, '').split('');
-    const arr2 = str2.toLowerCase().replace(/\s/g, '').split('');
-
-    return arr1.filter((char) => arr2.includes(char));
-  };
-
-  const getFlamesResult = (remainingLetters) => {
-    const flames = ['Friends', 'Lovers', 'Angry', 'Marriage', 'Engaged', 'Siblings'];
-
-    while (flames.length > 1) {
-      const index = (remainingLetters % flames.length - 1 + flames.length) % flames.length;
-      flames.splice(index, 1);
-    }
-
-    return flames[0];
-  };
-
-  const clearResult = () => {
-    setResult('');
-    setName1('');
-    setName2('');
-  };
+    const [name1, setName1] = useState('');
+    const [name2, setName2] = useState('');
+    const [result, setResult] = useState('');
+  
+    const calculateFlames = () => {
+      const flamesResult = getFlamesResult(name1, name2);
+      setResult(flamesResult);
+    };
+  
+    const getFlamesResult = (name1, name2) => {
+      if (!name1 || !name2) {
+        return "Please Enter valid input";
+      }
+  
+      const commonLetters = getCommonLetters(name1, name2);
+      const name1Remaining = removeCommonLetters(name1, commonLetters);
+      const name2Remaining = removeCommonLetters(name2, commonLetters);
+  
+      const remainingLettersSum = (name1Remaining.length + name2Remaining.length) % 6;
+  
+      switch (remainingLettersSum) {
+        case 1:
+          return "Friends";
+        case 2:
+          return "Love";
+        case 3:
+          return "Affection";
+        case 4:
+          return "Marriage";
+        case 5:
+          return "Enemy";
+        case 0:
+          return "Siblings";
+        default:
+          return "Invalid input";
+      }
+    };
+  
+    const getCommonLetters = (str1, str2) => {
+      const arr1 = str1.split('');
+      const arr2 = str2.split('');
+  
+      return arr1.filter(char => {
+        const index = arr2.indexOf(char);
+        if (index !== -1) {
+          arr2.splice(index, 1);
+          return true;
+        }
+        return false;
+      });
+    };
+  
+    const removeCommonLetters = (str, commonLetters) => {
+      return str.split('').filter(char => !commonLetters.includes(char)).join('');
+    };
+  
+    const clearResult = () => {
+      setResult('');
+      setName1('');
+      setName2('');
+    };
 
   return (
     <div>
